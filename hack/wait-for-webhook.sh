b2c45fd1960e766fabb23e20776dd5f1ed688023
Tino "Nox" Stöckel <itspandorasnox@gmail.com>
2019-07-02 16:43:40 +0200

    Update wait-for-webhook.sh

--- hack/wait-for-webhook.sh
+++ hack/wait-for-webhook.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "wait until mutating-webhook is running"
 NAMESPACE=mutating-webhook
-until [ $(kubectl -n ${NAMESPACE} get po 2> /dev/null | grep Running | wc -l) -eq 1 ]; do
+APP_SELECCTOR=app=default-container-resources
+
+echo "wait until mutating-webhook is running"
+until [ $(kubectl -n ${NAMESPACE} get po --selector=${APP_SELECCTOR} 2> /dev/null | grep Running | wc -l) -eq 1 ]; do
     echo -n "."
     sleep 1
 done
 echo "mutating-webhook is now running"
 
 echo "wait until mutating-webhook is ready"
-until [ $(kubectl -n ${NAMESPACE} get po 2> /dev/null | grep "1/1" | wc -l) -eq 1 ]; do
+until [ $(kubectl -n ${NAMESPACE} get po --selector=${APP_SELECCTOR} 2> /dev/null | grep "1/1" | wc -l) -eq 1 ]; do
     echo -n "."
     sleep 1
 done
